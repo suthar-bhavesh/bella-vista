e1edcdfe1580d379bd82249883bebc83a451c36e
Bhavesh Suthar <officialbhaveshsuthar>
2026-03-18 17:40:41 +0530

    contact information updated

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const UserFields = document.querySelectorAll('.userFields');
+    const Myform = document.querySelector('.myForm');
+    const InputError = document.querySelectorAll('.error');
+
+    Myform.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let isFormvalid = true;
+        let FormData = {};
+
+        UserFields.forEach((Input) => {
+
+            const value = Input.value.trim();
+            const FieldName = Input.getAttribute('name') || Input.placeholder;
+
+            if (value === "") {
+                isFormvalid = false;
+                Input.style.border = '2px solid red';
+            }
+            else {
+                FormData[FieldName] = value;
+                Input.style.border = '2px solid green';
+            }
+
+        })
+
+        if (isFormvalid) {
+            InputError.forEach((error) => {
+                error.textContent = "";
+
+                Myform.reset();
+            });
+
+        } else {
+            InputError.forEach((error) => {
+                error.textContent = "Please fill required fields";
+                error.style.border = "red";
+                return;
+            })
+        }
+    })
+})
